e9b475aa519c10f3670cc912cc4b596f036bc51d
Steven from WSL <steven.delarosa566@gmail.com>
2024-08-21 11:01:23 +0800

    added wait for db minimum command based in Django structure

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,10 @@
+"""
+Django command to wait for database to be avilable
+"""
+from django.core.management import BaseCommand
+
+class Command(BaseCommand):
+    """Django command to wait for database."""
+
+    def handle(self, *args, **options):
+        pass
