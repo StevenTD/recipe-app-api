7db27ab0c04f812dc43a46d1227de99cac4580d1
Steven from WSL <steven.delarosa566@gmail.com>
2024-08-21 15:50:29 +0800

    fix typos

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,5 +1,5 @@
 """
-Sample test
+Sample tests
 """
 
 from django.test import SimpleTestCase
@@ -7,16 +7,16 @@ from django.test import SimpleTestCase
 from app import calc
 
 
-class CalcTest(SimpleTestCase):
-    """Test the calc module"""
+class CalcTests(SimpleTestCase):
+    """Test the calc module."""
 
-    def test_add_numbner(self):
-        """Test adding number together."""
+    def test_add_number(self):
+        """Test adding numbers together."""
         res = calc.add(5, 6)
 
         self.assertEqual(res, 11)
 
-    def test_subtract_number(self):
+    def test_subtract_numbers(self):
         """Test subtracting numbers."""
         res = calc.subtract(10, 15)
 
