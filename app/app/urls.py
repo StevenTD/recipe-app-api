d313feaa7db19ebeb5816d27ffbe38b40dcdafcc
Steven from WSL <steven.delarosa566@gmail.com>
2024-10-05 18:24:54 +0800

    redirect to admin instead

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,9 +21,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.shortcuts import redirect
+
+
+def redirect_to_admin(request):
+    return redirect('/admin/')
+
 
 urlpatterns = [
-    path('', admin.site.urls),
+    path('', redirect_to_admin),
     path('admin/', admin.site.urls),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path(
